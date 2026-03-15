1b3a3431947a3ce53179cbd85efb30a7c4b66d15
Gokul <amudhangokul@gmail.com>
2026-03-16 00:40:33 +0530

    set up deployment backend

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://om-food-products.onrender.com/"],
+    allow_origins=["https://om-food-products.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
